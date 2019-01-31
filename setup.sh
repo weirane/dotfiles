889424e9379cd5da5c74c9936f25837faab5a9e9
Wang Ruochen <wangkevin920@163.com>
2019-01-31 15:53:26 +0800

    ftdetect for gitconfig

--- setup.sh
+++ setup.sh
@@ -12,6 +12,7 @@ done
 
 # .vim
 [[ -d ~/.vim/ftplugin ]] || ln -s ~/.dotfiles/vim/dotvim/ftplugin ~/.vim/ftplugin
+[[ -d ~/.vim/ftdetect ]] || ln -s ~/.dotfiles/vim/dotvim/ftdetect ~/.vim/ftdetect
 [[ -d ~/.vim/syntax ]] || ln -s ~/.dotfiles/vim/dotvim/syntax ~/.vim/syntax
 [[ -d ~/.vim/after ]] || ln -s ~/.dotfiles/vim/dotvim/after ~/.vim/after
 
---
+++ vim/dotvim/ftdetect/gitconfig.vim
@@ -0,0 +1,1 @@
+autocmd BufRead,BufNewFile gitconfig setfiletype gitconfig
