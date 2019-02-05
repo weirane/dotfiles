a98ec5de2e79fd65ff7afdaea6b16a3c5cbe3ab2
Wang Ruochen <wangkevin920@163.com>
2019-02-05 19:21:17 +0800

    fix the case where install fzf via git

--- zsh/apps/fzf.zsh
+++ zsh/apps/fzf.zsh
@@ -1,15 +1,9 @@
 fzf1=/usr/share/fzf
-fzf2=~/.fzf/shell
 
-kb=key-bindings.zsh
-cp=completion.zsh
-
-if [[ -f $fzf1/$kb && -f $fzf1/$cp ]]; then
+if [[ -f $fzf1/key-bindings.zsh && -f $fzf1/completion.zsh ]]; then
 	. $fzf1/key-bindings.zsh
 	. $fzf1/completion.zsh
-elif [[ -f $fzf2/$kb && -f $fzf2/$cp ]]; then
-	. $fzf2/key-bindings.zsh
-	. $fzf2/completion.zsh
+elif [[ -f ~/.fzf.zsh ]]; then
+	. ~/.fzf.zsh
 fi
-
-unset fzf1 fzf2 kb cp
+unset fzf1
