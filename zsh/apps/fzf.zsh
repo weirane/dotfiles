d27787678d55f2e6d43cb629e88c70d96586dc9e
Wang Ruochen <wangkevin920@163.com>
2019-02-05 18:36:07 +0800

    detect fzf in various places

--- zsh/apps/fzf.zsh
+++ zsh/apps/fzf.zsh
@@ -1,2 +1,15 @@
-. /usr/share/fzf/key-bindings.zsh
-. /usr/share/fzf/completion.zsh
+fzf1=/usr/share/fzf
+fzf2=~/.fzf/shell
+
+kb=key-bindings.zsh
+cp=completion.zsh
+
+if [[ -f $fzf1/$kb && -f $fzf1/$cp ]]; then
+	. $fzf1/key-bindings.zsh
+	. $fzf1/completion.zsh
+elif [[ -f $fzf2/$kb && -f $fzf2/$cp ]]; then
+	. $fzf2/key-bindings.zsh
+	. $fzf2/completion.zsh
+fi
+
+unset fzf1 fzf2 kb cp
