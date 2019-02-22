9577ddcba4ebbe1a3db04efd93a5dbb931d41bff
Wang Ruochen <wangkevin920@163.com>
2019-02-22 19:19:06 +0000

    remove pacman alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,7 +3,6 @@ alias df="df -h"
 alias ls="ls --color=auto"
 alias free="free -h"
 alias grep="grep --color=auto"
-alias pacman="pacman --color=always"
 
 alias tatt="tmux attach -t"
 alias tnew="tmux new -s"
