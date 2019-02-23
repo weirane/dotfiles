ba3e17b64ee522303b170c9e5495508d01f9652a
Wang Ruochen <wangkevin920@163.com>
2019-02-23 19:24:02 +0000

    only echo ln command

--- setup.sh
+++ setup.sh
@@ -4,16 +4,21 @@ if [[ ! -d ~/.dotfiles ]]; then
 	exit 1
 fi
 
-set -x
+echoln () {
+	set -x
+	ln "$@"
+	set +x
+}
+
 for f in ~/.dotfiles/symlinks/*; do
 	home_name=~/.$(basename $f)
 	[[ -f $home_name ]] || ln -s $f $home_name
 done
 
 # .vim
-[[ -d ~/.vim/ftplugin ]] || ln -s ~/.dotfiles/vim/dotvim/ftplugin ~/.vim/ftplugin
-[[ -d ~/.vim/ftdetect ]] || ln -s ~/.dotfiles/vim/dotvim/ftdetect ~/.vim/ftdetect
-[[ -d ~/.vim/syntax ]] || ln -s ~/.dotfiles/vim/dotvim/syntax ~/.vim/syntax
-[[ -d ~/.vim/after ]] || ln -s ~/.dotfiles/vim/dotvim/after ~/.vim/after
+[[ -d ~/.vim/ftplugin ]] || echoln -s ~/.dotfiles/vim/dotvim/ftplugin ~/.vim/ftplugin
+[[ -d ~/.vim/ftdetect ]] || echoln -s ~/.dotfiles/vim/dotvim/ftdetect ~/.vim/ftdetect
+[[ -d ~/.vim/syntax ]] || echoln -s ~/.dotfiles/vim/dotvim/syntax ~/.vim/syntax
+[[ -d ~/.vim/after ]] || echoln -s ~/.dotfiles/vim/dotvim/after ~/.vim/after
 
 exit 0
