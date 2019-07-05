dbe4792d3c7e954778c2599733e9ea90f2491c94
Wang Ruochen <wangkevin920@163.com>
2019-07-05 21:16:50 +0800

    use fd for fzf

--- zsh/apps/fzf.zsh
+++ zsh/apps/fzf.zsh
@@ -1,9 +1,15 @@
 fzf1=/usr/share/fzf
 
 if [[ -f $fzf1/key-bindings.zsh && -f $fzf1/completion.zsh ]]; then
-	. $fzf1/key-bindings.zsh
-	. $fzf1/completion.zsh
+    . $fzf1/key-bindings.zsh
+    . $fzf1/completion.zsh
 elif [[ -f ~/.fzf.zsh ]]; then
-	. ~/.fzf.zsh
+    . ~/.fzf.zsh
 fi
 unset fzf1
+
+if command -v fd >/dev/null; then
+    export FZF_DEFAULT_COMMAND='fd --type file --hidden --exclude .git/ --color=always'
+    export FZF_DEFAULT_OPTS='--ansi'
+    export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+fi
