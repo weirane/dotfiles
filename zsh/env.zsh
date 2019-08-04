89c7e862060c5da5afa1fb69d5ac90f892af17cf
Wang Ruochen <wangkevin920@163.com>
2019-08-04 22:01:30 +0800

    Move texlive to local

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,6 +1,1 @@
 export EDITOR="vim"
-
-# TeX Live
-export PATH=/usr/local/texlive/2018/bin/x86_64-linux:$PATH
-export MANPATH=/usr/local/texlive/2018/texmf-dist/doc/man:$MANPATH
-export INFOPATH=/usr/local/texlive/2018/texmf-dist/doc/info:$INFOPATH
