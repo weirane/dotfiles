0c1873723dc894d26d4f089ebd61e343080575be
Wang Ruochen <wangkevin920@163.com>
2019-10-25 09:22:49 +0800

    Add zsh plugin setups in ./setup.sh

--- setup.sh
+++ setup.sh
@@ -18,6 +18,14 @@ confirm () {
     esac
 }
 
+confirmy () {
+    read -rp "${1:-Are you sure? [Y/n]} " response
+    case "${response,,}" in
+        no|n) false ;;
+        *)    true  ;;
+    esac
+}
+
 # symlinks
 for f in ~/.dotfiles/symlinks/*; do
     home_name=~/.$(basename $f)
@@ -34,9 +42,39 @@ done
 [[ -d ~/.dotfiles/local ]] || echodo mkdir -p ~/.dotfiles/local
 [[ -f ~/.dotfiles/local/gdbinit ]] || echodo touch ~/.dotfiles/local/gdbinit
 
+# vim-plug
 if [[ ! -f ~/.vim/autoload/plug.vim ]] && confirm "Download plug.vim? (y/N)"; then
     echodo curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
+ZSH_CUSTOM="${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"
+# oh-my-zsh
+if [[ ! -d ~/.oh-my-zsh ]] && confirm "Setup oh-my-zsh? (y/N)"; then
+    tmpf=$(mktemp /tmp/.vim-plug-XXXXXXX.sh)
+    echodo curl -Lo $tmpf \
+        https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
+    less $tmpf
+    if confirmy "Proceed? (Y/n)"; then
+        echodo sh $tmpf
+        echodo rm $tmpf
+    fi
+fi
+
+# spaceship-prompt
+if [[ ! -d ~/.oh-my-zsh/custom/themes/spaceship-prompt ]] \
+        && confirm "Setup spaceship-prompt? (y/N)"; then
+    echodo git clone https://github.com/denysdovhan/spaceship-prompt.git \
+        "$ZSH_CUSTOM/themes/spaceship-prompt"
+    echodo ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" \
+        "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+fi
+
+# zsh-syntax-highlight
+if [[ ! -d ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]] \
+        && confirm "Setup zsh-syntax-highlighting? (y/N)"; then
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
+        "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
 fi
 
 exit 0
