9c1dc2fd399bba4cb10784d29e87eb47f60bff40
Wang Ruochen <wangkevin920@163.com>
2019-12-15 13:37:03 +0800

    Remove completion settings
    - wudao-dict is local
    - _rustup not needed any more

--- zsh/completion.zsh
+++
@@ -1,9 +0,0 @@
-# wudao-dict completion
-# from https://stackoverflow.com/questions/3249432/can-a-bash-tab-completion-script-be-used-in-zsh
-autoload bashcompinit
-bashcompinit
-[[ -f ~/git/wudao-dict/wudao-dict/wd_com ]] \
-    && \. ~/git/wudao-dict/wudao-dict/wd_com
-
-fpath+=$HOME/.dotfiles/zsh/completions
-compinit
