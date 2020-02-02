50f40aff853ac2e067406094b2f20745463c89c5
Wang Ruochen <wangkevin920@163.com>
2020-02-02 13:44:06 +0800

    Do not allow custom entries in mount and umount

--- dotconfig/i3/scripts/mount.sh
+++ dotconfig/i3/scripts/mount.sh
@@ -4,11 +4,11 @@
 
 chosen=$(lsblk -rnpo "name,label,size,type,mountpoint" |
     awk '/part $/ && !/WINRE_DRV/ { printf "%s %s (%s)\n",$1,$2,$3 }' |
-    rofi -dmenu -i -p "Mount which drive?" |
+    rofi -dmenu -no-custom -i -p "Mount which drive?" |
     cut -d' ' -f1)
 
 if [ $? = 0 ] && [ -n "$chosen" ]; then
-    msg=$(udisksctl mount -b $chosen)
+    msg=$(udisksctl mount -b $chosen 2>&1)
     notify-send '💻 Mount' "$msg"
 else
     echo "Abort"
--- dotconfig/i3/scripts/umount.sh
+++ dotconfig/i3/scripts/umount.sh
@@ -6,7 +6,7 @@ chosen=$(lsblk -nrpo "label,type,size,mountpoint" |
     awk '$2 == "part" && $4 !~ /\/boot|\/home$|SWAP/ && length($4) > 1 {
         printf "%s %s (%s)\n",$4,$1,$3
     }' |
-    rofi -dmenu -i -p "Unmount which drive?" |
+    rofi -dmenu -no-custom -i -p "Unmount which drive?" |
     cut -d' ' -f1)
 
 if [ $? = 0 ] && [ -n "$chosen" ]; then
