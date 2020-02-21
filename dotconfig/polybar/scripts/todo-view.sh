0f04354e6ef904597ea648179d130ba473490464
Wang Ruochen <wangkevin920@163.com>
2020-02-21 23:30:53 +0800

    Show the number of todos that due today

--- dotconfig/polybar/scripts/todo-view.sh
+++ dotconfig/polybar/scripts/todo-view.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+
+# Script for the todos module on my polybar. The total number of todos and the
+# number of todos that due today are shown.
+
 todo_file="$HOME/todo.txt"
 
 update() {
@@ -16,9 +20,16 @@ trap "show" USR1
 trap "update" USR2
 
 while true; do
-    # Count the todos. Blank lines and lines begin with "# " are ignored
-    num=$(grep -vE '^$|^# ' "$todo_file" -c)
-    echo "ToDo ($num)"
-    sleep infinity >/dev/null 2>&1 &
+    # Get the todos. Blank lines and lines begin with "# " are ignored
+    todos=$(grep -vE '^$|^# ' "$todo_file")
+    num=$(echo "$todos" | wc -l)
+    today=$(echo "$todos" | grep "($(date +%m-%d))$" -c)
+    if [ "$today" -gt 0 ]; then
+        today=", $today"
+    else
+        today=""
+    fi
+    echo "ToDo ($num$today)"
+    sleep 2h >/dev/null 2>&1 &
     wait
 done
