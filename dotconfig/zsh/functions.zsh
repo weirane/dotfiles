944ce5f41d3a97fe1d6892aa9d823e76526f0e68
Wang Ruochen <wangkevin920@163.com>
2020-03-12 12:02:30 +0800

    Add lzma support in ex

--- dotconfig/zsh/functions.zsh
+++ dotconfig/zsh/functions.zsh
@@ -25,23 +25,24 @@ function df () {
 
 # ex - archive extractor. usage: ex <file>
 function ex () {
-    if [[ ! -f $1 ]]; then
+    if [[ ! -f "$1" ]]; then
         echo "'$1' is not a valid file"
         return
     fi
-    case $1 in
-        *.tar.bz2) tar xjf $1   ;;
-        *.tar.gz)  tar xzf $1   ;;
-        *.tar.xz)  tar xJf $1   ;;
-        *.bz2)     bunzip2 $1   ;;
-        *.rar)     unrar x $1   ;;
-        *.gz)      gunzip $1    ;;
-        *.tar)     tar xf $1    ;;
-        *.tbz2)    tar xjf $1   ;;
-        *.tgz)     tar xzf $1   ;;
-        *.zip)     unzip $1     ;;
-        *.Z)       uncompress $1;;
-        *.7z)      7z x $1      ;;
+    case "$1" in
+        *.tar.bz2) tar xjf "$1"   ;;
+        *.tar.gz)  tar xzf "$1"   ;;
+        *.tar.xz)  tar xJf "$1"   ;;
+        *.lzma)    unlzma "$1"    ;;
+        *.bz2)     bunzip2 "$1"   ;;
+        *.rar)     unrar x "$1"   ;;
+        *.gz)      gunzip "$1"    ;;
+        *.tar)     tar xf "$1"    ;;
+        *.tbz2)    tar xjf "$1"   ;;
+        *.tgz)     tar xzf "$1"   ;;
+        *.zip)     unzip "$1"     ;;
+        *.Z)       uncompress "$1";;
+        *.7z)      7z x "$1"      ;;
         *)         echo "'$1' cannot be extracted via ex()";;
     esac
 }
