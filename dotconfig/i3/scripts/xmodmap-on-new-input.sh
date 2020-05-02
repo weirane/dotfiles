7bb4b047e81a38c964233af846ad4dbcf10dd0fc
Wang Ruochen <wangkevin920@163.com>
2020-05-02 12:31:21 +0800

    Don't run xcape upon new device

--- dotconfig/i3/scripts/xmodmap-on-new-input.sh
+++ dotconfig/i3/scripts/xmodmap-on-new-input.sh
@@ -11,15 +11,11 @@ pgrep -f "inotifywait --quiet --event create --exclude \.\*tmp\.\* /dev/input" &
 
 [ ! -f ~/.Xmodmap ] && exit
 
-has_xcape=0
-command -v xcape >/dev/null && has_xcape=1
-
 notify-send --expire-time=5000 "xmodmap-on-new-input" "Enabled"
 
 while true; do
     inotifywait --quiet --event create --exclude '.*tmp.*' /dev/input
     sleep 1
     xmodmap ~/.Xmodmap
-    [ "$has_xcape" = 1 ] && xcape -e 'Control_L=Escape'
     notify-send --urgency=low --expire-time=2500 "New device" "xmodmap run"
 done
