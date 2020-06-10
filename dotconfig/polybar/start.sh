f12969336c1a824519393ef2df38f27e25e4d44e
Wang Ruochen <wangkevin920@163.com>
2020-06-10 20:29:59 +0800

    More efficient polybar starting script

--- dotconfig/polybar/start.sh
+++ dotconfig/polybar/start.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 # Terminate already running bar instances
-killall --quiet polybar
+killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep --euid "$(id -ru)" --exact polybar >/dev/null; do sleep 1; done
+while pgrep -U "$(id -ru)" -x polybar >/dev/null; do sleep 1; done
 
 # Launch polybar on all the available monitors
 polybar --list-monitors | while read -r line; do
-    moni=$(echo "$line" | cut -d':' -f1)
-    if echo "$line" | grep 'primary'; then
+    moni=${line%%:*}
+    if expr "$line" : '.*primary' >/dev/null; then
         MONITOR="$moni" TRAY_POS=right polybar -r top &
     else
         MONITOR="$moni" polybar -r top &
