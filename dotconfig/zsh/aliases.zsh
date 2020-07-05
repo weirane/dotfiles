9c4084dfe21590e33cf96de8d4a6d0d0ae383ca9
Wang Ruochen <wangkevin920@163.com>
2020-07-05 19:59:10 +0800

    More aliases

--- dotconfig/zsh/aliases.zsh
+++ dotconfig/zsh/aliases.zsh
@@ -13,11 +13,15 @@ alias tnew="tmux new -s"
 alias gdb="gdb -q"
 alias bc="bc -lq"
 
-alias rsbk="RUST_BACKTRACE=1"
-alias langzh="LANG=zh_CN.UTF-8"
+alias rsbk="env RUST_BACKTRACE=1"
+alias langzh="env LANG=zh_CN.UTF-8"
 
 alias pys="python3 -m http.server"
 
 alias :q="exit"
+alias :e="${EDITOR:-vim}"
 
 alias -s {pdf,png,jpg,jpeg,gif,svg,html}="open"
+alias -s {mov,mkv,flv,mp4,rmvb}="open"
+alias -s {m4a,ogg,mp3,aac,wav}="open"
+alias -s zip="open"
