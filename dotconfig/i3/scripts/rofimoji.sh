bd3ab39f8e45e83fe04e219889bf870a7157f93d
Wang Ruochen <wangkevin920@163.com>
2020-07-12 09:37:10 +0800

    More efficient emoji script

--- dotconfig/i3/scripts/rofimoji.sh
+++ dotconfig/i3/scripts/rofimoji.sh
@@ -2,9 +2,6 @@
 
 # Choose an emoji from a rofi menu, copy it to the clipboard
 
-# Must have xclip installed to even show menu.
-command -v xclip >/dev/null || exit
-
 emojis=$(realpath "$(dirname "$0")")/emojis
 
 [ ! -f "$emojis" ] && exit
@@ -13,5 +10,5 @@ chosen=$(rofi -dmenu -i -no-custom -markup-rows -p "Emoji" < "$emojis")
 
 [ -z "$chosen" ] && exit
 
-echo "$chosen" | awk '{ printf "%s",$1 }' | xclip -selection clipboard
+printf %s "${chosen%% *}" | xclip -selection clipboard
 notify-send 'rofimoji' "'$(xclip -o -selection clipboard)' copied to clipboard."
