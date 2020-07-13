6ec8ad1838ea00132b9082ee765632488d68cccf
Wang Ruochen <wangkevin920@163.com>
2020-07-13 13:50:05 +0800

    Only run todo-view on primary display

--- dotconfig/polybar/start.sh
+++ dotconfig/polybar/start.sh
@@ -10,7 +10,7 @@ while pgrep -U "$(id -ru)" -x polybar >/dev/null; do sleep 1; done
 polybar --list-monitors | while read -r line; do
     moni=${line%%:*}
     if expr "$line" : '.*primary' >/dev/null; then
-        MONITOR="$moni" TRAY_POS=right polybar -r top &
+        MONITOR="$moni" PRIMARY=1 TRAY_POS=right polybar -r top &
     else
         MONITOR="$moni" polybar -r top &
     fi
