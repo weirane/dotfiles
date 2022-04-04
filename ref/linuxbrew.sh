1a6e597900fcfc83d65a087bd5584842d513e7f7
Wang Ruochen <wrc@ruo-chen.wang>
2022-04-03 20:20:10 -0700

    Add linuxbrew stuff

---
+++ ref/linuxbrew.sh
@@ -0,0 +1,14 @@
+# linuxbrew settings
+
+# go in .zshenv:
+user=$USER
+HOMEBREW_PREFIX="/home/$user/.linuxbrew";
+HOMEBREW_CELLAR="/home/$user/.linuxbrew/Cellar";
+HOMEBREW_REPOSITORY="/home/$user/.linuxbrew/Homebrew";
+PATH="/home/$user/.linuxbrew/bin:/home/$user/.linuxbrew/sbin${PATH+:$PATH}";
+MANPATH="/home/$user/.linuxbrew/share/man${MANPATH+:$MANPATH}:";
+INFOPATH="/home/$user/.linuxbrew/share/info:${INFOPATH:-}";
+unset user
+
+# go in zshrc:
+export HOMEBREW_NO_ENV_HINTS=1
