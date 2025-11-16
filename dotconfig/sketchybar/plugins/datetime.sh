267e6c6c08a1605c10b1cffb0320ebde394fa897
Ruochen Wang <wrc@ruo-chen.wang>
2025-11-15 19:58:29 -0500

    small updates

--- dotconfig/sketchybar/plugins/datetime.sh
+++ dotconfig/sketchybar/plugins/datetime.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 
 export LC_ALL=fr_FR.UTF-8
-sketchybar --set $NAME label="$(date '+%a. %e %B %H:%M' | tr A-Z a-z)"
+sketchybar --set $NAME label="$(date '+%a %e %B %H:%M' | tr A-Z a-z)"
